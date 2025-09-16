edb97fd9d05445104ed5ccef80a2041f3f098a2e
Matheus Nagao <matheusshimoda325@gmail.com>
2025-09-15 22:40:37 -0300

    Adicionada a página de Iniciar roteiro Masp Raiz

---
+++ app/IniciarMasp/page.js
@@ -0,0 +1,5 @@
+import RaizFinal from "./raizFinal"
+
+export default function Page() {
+  return <RaizFinal />;
+}
---
+++ app/IniciarMasp/raizFinal.js
@@ -0,0 +1,74 @@
+"use client";
+import { useCallback } from "react";
+import { Button, Typography, Box } from "@mui/material";
+import Image from "next/image";
+import Body from "../../components/body";
+import styles from "./raizFinal.module.css";
+
+const IniciarRoteiro20 = () => {
+  const onVoltarIconClick = useCallback(() => {
+    // Please sync "Seleção de Tipo de Roteiro 2.0" to the project
+  }, []);
+
+  const onBotoIniciarRoteiroClick = useCallback(() => {
+    window.open(
+      "https://www.google.com/maps/dir/?api=1&origin=My+Location&destination=Mirante+9+de+Julho&waypoints=Museu+de+Arte+de+São+Paulo|Charme+da+Paulista+Restaurante"
+    );
+  }, []);
+
+  return (
+    <Box className={styles.iniciarRoteiro20}>
+      <section className={styles.imagemHero}>
+        <Image
+          className={styles.voltarIcon}
+          width={24}
+          height={38}
+          sizes="100vw"
+          alt=""
+          src="/Voltar.svg"
+          onClick={onVoltarIconClick}
+        />
+      </section>
+      <Body />
+      <section
+        className={styles.botoIniciarRoteiro}
+        onClick={onBotoIniciarRoteiroClick}
+      >
+        <Typography
+          className={styles.iniciarRotaCom}
+          variantMapping={{ inherit: "Button" }}
+        >
+          Iniciar rota com Google
+        </Typography>
+      </section>
+      <section className={styles.ttulo}>
+        <Box className={styles.iniciarRoteiro20Ttulo}>
+          <Typography
+            className={styles.masp}
+            variant="inherit"
+            variantMapping={{ inherit: "h1" }}
+            sx={{ fontWeight: "700", lineHeight: "4.375rem" }}
+          >
+            MASP
+          </Typography>
+          <Typography
+            className={styles.museuDeArte}
+            variant="inherit"
+            variantMapping={{ inherit: "h3" }}
+            sx={{ fontWeight: "600", fontSize: "var(--font-size-20)" }}
+          >
+            Museu de Arte de São Paulo
+          </Typography>
+        </Box>
+        <Box className={styles.descrio}>
+          <div className={styles.iniciarRotaCom}>1957-1968 Lina Bo Bardi</div>
+          <div className={styles.avenidaPaulista1578}>
+            Avenida Paulista, 1578 Cerqueira César
+          </div>
+        </Box>
+      </section>
+    </Box>
+  );
+};
+
+export default IniciarRoteiro20;
