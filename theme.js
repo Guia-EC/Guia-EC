abde37f8b91a4235b34b614cca71cec0db45a662
Douglas-Rochaa <rocha.dougg@gmail.com>
2025-09-15 23:02:10 -0300

    Ajuste de tela de usuários, inserção de fonte global poppins

--- theme.js
+++ theme.js
@@ -1,6 +1,14 @@
 "use client";
 import { createTheme } from "@mui/material/styles";
 
-const muiTheme = createTheme({});
+const muiTheme = createTheme({
+  // Adicione esta seção de tipografia
+  typography: {
+    fontFamily: [
+      'Poppins',      // Sua fonte principal
+      'sans-serif',   // Fonte de segurança
+    ].join(','),
+  },
+});
 
 export default muiTheme;
