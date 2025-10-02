785baecd9626ec1032dd8aeb78a5dcc4e5dd9dc5
Douglas-Rochaa <rocha.dougg@gmail.com>
2025-10-02 00:08:31 -0300

    Ajuste pendente

--- app/layout.js
+++ app/layout.js
@@ -18,9 +18,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </head>
       <body>
         {/* 2. Envolva todos os outros providers com o AuthProvider */}
         <AuthProvider>
