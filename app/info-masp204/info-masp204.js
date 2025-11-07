4c52fdd2e90c4fc0b27d1069f918192371b2ac76
Douglas-Rochaa <rocha.dougg@gmail.com>
2025-11-06 22:53:15 -0300

    Ajuste botão info-masp

--- app/info-masp204/info-masp204.js
+++ app/info-masp204/info-masp204.js
@@ -26,7 +26,7 @@ const InfoMasp204 = () => {
           variantMapping={{ inherit: "h3" }}
           sx={{ fontWeight: "400" }}
         >
-          Fazer um roteiro pelo MASP
+          Fazer Roteiro
         </Typography> 
       </section>
       <ScrollToTopButton />
